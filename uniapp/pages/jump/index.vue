6dad4e69505cc89d27e4c0889b4c2da07db8c34b
yangqinlong <2214409512@qq.com>
2023-02-17 19:01:15 +0800

    1

---
+++ uniapp/pages/jump/index.vue
@@ -0,0 +1,41 @@
+<!-- 设置 -->
+<template>
+  <view>
+   
+  </view>
+</template>
+
+<script>
+import { mapState, mapActions, mapGetters } from "vuex";
+export default {
+  components: {},
+  data() {
+    return {
+        token: uni.getStorageSync('token')
+    };
+  },
+ computed: {
+ ...mapGetters(['initShop', 'homeTemplate', 'hasTemplate', 'isLogin', 'userInfo' ])
+ },
+  onShow() {
+  	console.log("this.authType=====",typeof this.token)
+  			 
+  	if (this.token == '') {
+  		uni.navigateTo({
+  			url: "/pages/user/login"
+  		}) 
+  	}else{
+		uni.switchTab({
+			url:"/pages/index/index"
+		})
+	}
+  },
+ 
+  methods: { 
+	  
+  },
+};
+</script>
+
+<style lang="scss"> 
+</style>
